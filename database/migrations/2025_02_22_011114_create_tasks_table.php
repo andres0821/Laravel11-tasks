d227be97d370db6cb1c32bfcb6795cd0c024f7ba
andres0821 <acgilserna@gmail.com>
2025-02-23 15:37:37 -0500

    feat: :sparkles:

--- database/migrations/2025_02_22_011114_create_tasks_table.php
+++ database/migrations/2025_02_22_011114_create_tasks_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
+            //add deleted_at column
+            $table->softDeletes();
         });
     }
 
